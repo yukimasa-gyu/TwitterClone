f8ea274444d8b998c435c924a2add7651b9ffc53
yukimasa-gyu <shiramasa0925@gmail.com>
2021-07-25 20:14:04 +0900

    その他の機能追加

--- search.php
+++ search.php
@@ -1,8 +1,10 @@
 <?php
 // 設定関連を読み込む
 include_once('./config.php');
-// 便利な関数を読み込む.
+// 便利な関数を読み込む
 include_once('./utill.php');
+
+
  
 ///////////////////////////////////////
 // ツイート一覧
@@ -33,45 +35,35 @@ $view_tweets = [
 ];
  
 ?>
-
 <!DOCTYPE html>
 <html lang="ja">
  
 <head>
     <?php include_once('./vies/common/head.php'); ?>
-    <title>ホーム画面 / Twitterクローン</title>
-    <meta name="description" content="ホーム画面です">
+    <title>検索画面 / Twitterクローン</title>
+    <meta name="description" content="検索画面です">
 </head>
  
-<body class="home">
+<body class="home search text-center">
     <div class="container">
         <?php include_once('./vies/common/side.php'); ?>
  
         <div class="main">
             <div class="main-header">
-                <h1>ホーム</h1>
+                <h1>検索</h1>
             </div>
-            <div class="tweet-post">
-                <div class="my-icon">
-                    <img src="<?php echo HOME_URL; ?>vies/img_uploaded/user/sample-person.jpg" alt="">
+ 
+            <form action="search.php" method="get">
+                <div class="search-area">
+                    <input type="text" class="form-control" placeholder="キーワード検索" name="keyword" value="">
+                    <button type="submit" class="btn">検索</button>
                 </div>
-                <div class="input-area">
-                    <form action="post.php" method="post" enctype="multipart/form-data">
-                        <textarea name="body" placeholder="いまどうしてる？" maxlength="140"></textarea>
-                        <div class="bottom-area">
-                            <div class="mb-0">
-                                <input type="file" name="image" class="form-control form-control-sm">
-                            </div>
-                            <button class="btn" type="submit">つぶやく</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            </form>
  
             <div class="ditch"></div>
  
             <?php if (empty($view_tweets)) : ?>
-                <p class="p-3">ツイートがまだありません</p>
+                <p class="p-3">該当のツイートは見つかりませんでした</p>
             <?php else : ?>
                 <div class="tweet-list">
                     <?php foreach ($view_tweets as $view_tweet) : ?>
---
+++ vies/common/side.php
@@ -0,0 +1,13 @@
+<div class="side">
+            <div class="side-inner">
+                <ul class="nav flex-column">
+                    <li class="nav-item"><a href="twitter.php" class="nav-link"><img src="<?php echo HOME_URL; ?>vies/img/logo-twitterblue.svg" alt="" class="icon"></a></li>
+                    <li class="nav-item"><a href="twitter.php" class="nav-link"><img src="<?php echo HOME_URL; ?>vies/img/icon-home.svg" alt=""></a></li>
+                    <li class="nav-item"><a href="search.php" class="nav-link"><img src="<?php echo HOME_URL; ?>vies/img/icon-search.svg" alt=""></a></li>
+                    <li class="nav-item"><a href="notification.php" class="nav-link"><img src="<?php echo HOME_URL; ?>vies/img/icon-notification.svg" alt=""></a></li>
+                    <li class="nav-item"><a href="profile.php" class="nav-link"><img src="<?php echo HOME_URL; ?>vies/img/icon-profile.svg" alt=""></a></li>
+                    <li class="nav-item"><a href="post.php" class="nav-link"><img src="<?php echo HOME_URL; ?>vies/img/icon-post-tweet-twitterblue.svg" alt="" class="post-tweet"></a></li>
+                    <li class="nav-item my-icon"><img src="<?php echo HOME_URL; ?>vies/img_uploaded/user/sample-person.jpg" alt=""></li>
+                </ul>
+            </div>
+        </div>
